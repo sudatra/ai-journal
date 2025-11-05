afd89bf8d89d69145c656bd5ab960748c1d7dc52
sudatra <mail2sudatra@gmail.com>
2025-11-05 20:07:52 +0530

    added modal dialog context

--- context/ModalContext.tsx
+++ context/ModalContext.tsx
@@ -1,5 +1,6 @@
+import { AlertTriangle, Info } from "@tamagui/lucide-icons";
 import { createContext, ReactNode, useState } from "react";
-import { Dialog, DialogContent, DialogOverlay } from "tamagui";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogOverlay, AlertDialogTitle, Button, Dialog, DialogClose, DialogContent, DialogDescription, DialogOverlay, DialogTitle, XStack, YStack } from "tamagui";
 
 type ModalType = 'alert' | 'dialog';
 
@@ -85,9 +86,159 @@ export function ModalProvider({ children }: { children: ReactNode }) {
               exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
               gap="$4"
             >
-              
+              <YStack
+                gap={'$3'}
+                style={{ alignItems: 'center' }}
+              >
+                <Info 
+                  size={48}
+                  color={'$purple10'}
+                />
+
+                <DialogTitle style={{ textAlign: 'center' }}>
+                  {modalConfig.title}
+                </DialogTitle>
+              </YStack>
+
+              {
+                modalConfig.description && (
+                  <DialogDescription style={{ textAlign: 'center' }}>
+                    {modalConfig.description}
+                  </DialogDescription>
+                )
+              }
+
+              {modalConfig.content}
+
+              <XStack
+                pb={'$4'}
+                gap={'$3'}
+                style={{ justifyContent: 'center' }}
+              >
+                <DialogClose
+                  displayWhenAdapted
+                  asChild
+                >
+                  <Button
+                    onPress={handleConfirm}
+                    flex={1}
+                  >
+                    {modalConfig.cancelText || 'Cancel'}
+                  </Button>
+                </DialogClose>
+
+                {
+                  modalConfig.onConfirm && (
+                    <DialogClose
+                      displayWhenAdapted
+                      asChild
+                    >
+                      <Button
+                        onPress={handleConfirm}
+                        flex={1}
+                      >
+                        {modalConfig.confirmText || 'Confirm'}
+                      </Button>
+                    </DialogClose>
+                  )
+                }
+              </XStack>
             </DialogContent>
           </Dialog>
+        )
+      }
+
+      {
+        modalConfig?.type === 'alert' && (
+          <AlertDialog
+            modal={true}
+            open={open}
+            onOpenChange={setOpen}
+          >
+            <AlertDialogOverlay 
+              key={'overlay'}
+              animation={'lazy'}
+              opacity={0.5}
+              enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+              exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+              onPress={hideModal}
+            />
+
+            <AlertDialogContent
+              bordered={true}
+              elevate={true}
+              key="content"
+              animation={[
+                "quick",
+                {
+                  opacity: {
+                    overshootClamping: true,
+                  }
+                }
+              ]}
+              enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+              exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+              gap="$4"
+            >
+              <YStack
+                gap={'$3'}
+                style={{ alignItems: 'center' }}
+              >
+                <AlertTriangle 
+                  size={48}
+                  color={'$red10'}
+                />
+
+                <AlertDialogTitle style={{ textAlign: 'center' }}>
+                  {modalConfig.title}
+                </AlertDialogTitle>
+              </YStack>
+
+              {
+                modalConfig.description && (
+                  <AlertDialogDescription style={{ textAlign: 'center' }}>
+                    {modalConfig.description}
+                  </AlertDialogDescription>
+                )
+              }
+
+              {modalConfig.content}
+
+              <XStack
+                pb={'$4'}
+                gap={'$3'}
+                style={{ justifyContent: 'center' }}
+              >
+                <AlertDialogCancel
+                  displayWhenAdapted
+                  asChild
+                >
+                  <Button
+                    onPress={handleConfirm}
+                    flex={1}
+                  >
+                    {modalConfig.cancelText || 'Cancel'}
+                  </Button>
+                </AlertDialogCancel>
+
+                {
+                  modalConfig.onConfirm && (
+                    <AlertDialogAction
+                      displayWhenAdapted
+                      asChild
+                    >
+                      <Button
+                        onPress={handleConfirm}
+                        flex={1}
+                      >
+                        {modalConfig.confirmText || 'Confirm'}
+                      </Button>
+                    </AlertDialogAction>
+                  )
+                }
+              </XStack>
+            </AlertDialogContent>
+          </AlertDialog>
         )
       }
     </ModalContext.Provider>
