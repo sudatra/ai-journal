b88e424f09b2ee52f92659ca0a33477d2f59de90
sudatra <mail2sudatra@gmail.com>
2025-11-06 20:04:13 +0530

    added stack screens

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,35 @@
+import { useAuth } from '@clerk/clerk-expo';
+import { Stack } from 'expo-router';
+import React from 'react';
+import { Spinner } from 'tamagui';
+
+export default function AppLayout() {
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if(!isLoaded) {
+    return (
+      <Spinner />
+    )
+  }
+
+  return (
+    <Stack>
+      <Stack.Protected guard={isSignedIn}>
+        <Stack.Screen 
+          name='(tabs)' 
+          options={{ headerShown: false }}
+        />
+      </Stack.Protected>
+
+      <Stack.Screen 
+        name='sign-in' 
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen 
+        name='sign-up' 
+        options={{ headerShown: false }}
+      />
+    </Stack>
+  )
+}
